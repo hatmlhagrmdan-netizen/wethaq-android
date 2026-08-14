400427e6fc838c66e87dff46beeaf67a3116970e
hatmlhagrmdan-netizen <hatmlhagrmdan@gmail.com>
2026-08-15 02:33:53 +0300

    Bump Wethaq app version to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.wethaq.app"
         minSdk = 23
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.2.0"
     }
 
     val keystorePath = System.getenv("WETHAQ_KEYSTORE")
