58594ea02e9efcb99427996e56e677fa34fbe1ec
hatmlhagrmdan-netizen <hatmlhagrmdan@gmail.com>
2026-08-15 03:08:12 +0300

    رفع إصدار وَثاق إلى 1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.wethaq.app"
         minSdk = 23
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.3.0"
+        versionCode = 4
+        versionName = "1.4.0"
     }
 
     val keystorePath = System.getenv("WETHAQ_KEYSTORE")
