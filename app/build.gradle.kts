a2447ad8e581622999f3e6159ab132599b9a1efe
hatmlhagrmdan-netizen <hatmlhagrmdan@gmail.com>
2026-08-16 21:54:36 +0300

    chore: remove duplicate okhttp dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,5 +52,4 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
