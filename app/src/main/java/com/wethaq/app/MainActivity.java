3ab2c6bf5a4a8848491fae3fb507d7fdfcdba74c
hatmlhagrmdan-netizen <hatmlhagrmdan@gmail.com>
2026-08-22 19:49:15 +0300

    استعادة

--- app/src/main/java/com/wethaq/app/MainActivity.java
+++ app/src/main/java/com/wethaq/app/MainActivity.java
@@ -1,8 +1,4 @@
 package com.wethaq.app;
-
 import android.app.Activity;
 import android.os.Bundle;
-
-public final class MainActivity extends Activity {
- @Override protected void onCreate(Bundle b){super.onCreate(b);}
-}
+public final class MainActivity extends Activity { @Override protected void onCreate(Bundle b){super.onCreate(b);} }
