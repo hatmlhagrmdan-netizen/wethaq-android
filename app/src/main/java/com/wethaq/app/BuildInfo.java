a57c0419d6fa4916542212fced18da600ba0e170
hatmlhagrmdan-netizen <hatmlhagrmdan@gmail.com>
2026-08-23 23:25:34 +0300

    Prepare Wethaq functional black-gold UI build 2.2.1

---
+++ app/src/main/java/com/wethaq/app/BuildInfo.java
@@ -0,0 +1,8 @@
+package com.wethaq.app;
+
+/** Build marker for the production Wethaq UI release. */
+public final class BuildInfo {
+    public static final String VERSION = "2.2.1";
+    public static final String UI = "black-gold-functional";
+    private BuildInfo() {}
+}
