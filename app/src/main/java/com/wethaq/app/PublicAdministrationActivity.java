6371e08c1f32a3d3b8f6215680b67b55466b79e9
hatmlhagrmdan-netizen <hatmlhagrmdan@gmail.com>
2026-09-23 19:52:21 +0300

    إكمال استيرادات لوحة الإدارة

--- app/src/main/java/com/wethaq/app/PublicAdministrationActivity.java
+++ app/src/main/java/com/wethaq/app/PublicAdministrationActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.widget.Button;
