d65704385cfc8830d04055f637edde530fca9119
hatmlhagrmdan-netizen <hatmlhagrmdan@gmail.com>
2026-09-23 13:16:54 +0300

    统一生产接口地址并同步 إصدار التطبيق

--- app/src/main/java/com/wethaq/app/BuildInfo.java
+++ app/src/main/java/com/wethaq/app/BuildInfo.java
@@ -2,7 +2,7 @@ package com.wethaq.app;
 
 /** Build marker for the Wethaq release. */
 public final class BuildInfo {
-    public static final String VERSION = "2.4.0";
+    public static final String VERSION = "2.5.0";
     public static final String UI = "black-gold-functional-admin-messaging";
     private BuildInfo() {}
 }
